fd9d857024adef90b954aaa934d2204a5bbf3f75
René Burghardt <rburghardt@riege.com>
2021-11-25 15:24:06 +0100

    ✨ Initial commit

---
+++ docker-build/rpmbuild.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+KEYFILE=/rpmbuild/rpm_signing_key.gpg
+KEYID="$(gpg --with-colons "$KEYFILE" | head -n1 | cut -d : -f 5)"
+
+tar -czvf /rpmbuild/SOURCES/"$RPM_NAME".tgz -C /rpmbuild/SOURCES/ .
+
+echo "KeyId: $KEYID"
+
+gpg --import "$KEYFILE"
+echo "
+%_signature gpg
+%_gpg_name $KEYID
+%_topdir /rpmbuild
+%release $GITHUB_RUN_NUMBER
+%name $RPM_NAME
+%version $GITHUB_REF_NAME
+%debug_package %{nil}
+" > ~/.rpmmacros
+
+rpmbuild --undefine=_disable_source_fetch -bb /rpmbuild/SPECS/"$RPM_NAME".spec
+
+RPMFILE=$(find "/rpmbuild/RPMS" -name "*.rpm")
+
+/usr/bin/expect -df /rpmexpect "$RPMFILE" "$RPM_SIGN_KEY_PASSPHRASE"
