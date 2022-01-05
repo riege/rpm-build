5771e98e7bd733b2af5b0b56b914519e49152f95
René Burghardt <rburghardt@riege.com>
2022-01-05 15:34:39 +0100

    Unifing sign of RPM file

--- docker-build/rpmbuild.sh
+++ docker-build/rpmbuild.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 
-KEYFILE=/rpmbuild/rpm_signing_key.gpg
-KEYID="$(gpg --with-colons "$KEYFILE" | head -n1 | cut -d : -f 5)"
+rpmbuild --undefine=_disable_source_fetch -bb /rpmbuild/SPECS/"$RPM_NAME".spec || exit 1
+RPMFILE=$(find "/rpmbuild/RPMS" -name "*.rpm") || exit 1
 
-gpg --import "$KEYFILE"
-echo "
-%_signature gpg
-%_gpg_name $KEYID
-%_topdir /rpmbuild
-%release $RPM_RELEASE
-%name $RPM_NAME
-%version $RPM_VERSION
-%debug_package %{nil}
-" > ~/.rpmmacros
-
-rpmbuild --undefine=_disable_source_fetch -bb /rpmbuild/SPECS/"$RPM_NAME".spec
-
-RPMFILE=$(find "/rpmbuild/RPMS" -name "*.rpm")
-
-/usr/bin/expect -df /rpmexpect "$RPMFILE" "$RPM_SIGN_KEY_PASSPHRASE"
+chmod 666 "$RPMFILE"
