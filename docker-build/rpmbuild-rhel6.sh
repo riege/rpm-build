65dfc97d2189f97baa90e4de609b12e8f753a386
Oliver Wiebeck <61313842+wiebeck@users.noreply.github.com>
2022-01-27 14:10:51 +0100

    Support for RHEL 6 (#15)

---
+++ docker-build/rpmbuild-rhel6.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# need to change ownership because of "Bad owner/group" error otherwise
+chown -R root:root /rpmbuild
+rpmbuild -bb /rpmbuild/SPECS/"$RPM_NAME".spec || exit 1
+RPMFILE=$(find "/rpmbuild/RPMS" -name "*.rpm") || exit 1
+
+chmod 666 "$RPMFILE"
