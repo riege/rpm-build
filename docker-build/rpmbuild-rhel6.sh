e0a140f72ecc0083d0eff7e51b6bbe91d9d9d80a
Oliver Wiebeck <wiebeck@riege.com>
2022-01-27 12:41:33 +0100

    finalize changes

--- docker-build/rpmbuild-rhel6.sh
+++ docker-build/rpmbuild-rhel6.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-set -x
-
-ls -la /rpmbuild/SOURCES
-chown -R root:root /rpmbuild/SOURCES
+# need to change ownership because of "Bad owner/group" error otherwise
+chown -R root:root /rpmbuild
 rpmbuild -bb /rpmbuild/SPECS/"$RPM_NAME".spec || exit 1
 RPMFILE=$(find "/rpmbuild/RPMS" -name "*.rpm") || exit 1
 
