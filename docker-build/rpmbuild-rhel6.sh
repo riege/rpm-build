7f9252f370a332a2bf7550a4fd7b50371b0beca9
René Burghardt <rburghardt@riege.com>
2023-06-21 12:09:45 +0200

    🚀 Download sources with spectools before build (#35)

--- docker-build/rpmbuild-rhel6.sh
+++ docker-build/rpmbuild-rhel6.sh
@@ -2,6 +2,7 @@
 
 # need to change ownership because of "Bad owner/group" error otherwise
 chown -R root:root /rpmbuild
+spectool -g -R /rpmbuild/SPECS/"$RPM_NAME".spec || exit 1
 rpmbuild -bb /rpmbuild/SPECS/"$RPM_NAME".spec || exit 1
 RPMFILE=$(find "/rpmbuild/RPMS" -name "*.rpm") || exit 1
 
